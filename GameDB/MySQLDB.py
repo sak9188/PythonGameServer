10ab18d6caddc7337668183943f384d49a7bff0d
HK <sak9188@163.com>
2021-04-21 20:15:39 +0800

    校验DB表和配置库

--- GameDB/MySQLDB.py
+++ GameDB/MySQLDB.py
@@ -1,26 +1,41 @@
 # -*- coding: UTF-8 -*-
 
-import DataType
+from DataType import DBDataType
+import collections
+
 
 class DBColumn(object):
-	__slots__ = ["name", "data_type", "nullable", "auto_increment", "prikey"]
+	__slots__ = ["name", "data_type", "nullable", "default", "auto_increment", "unique", "prikey", "comment"]
 
-	def __init__(self, name, data_type, nullable=True, auto_increment=True, prikey=True):
+	def __init__(self, name, data_type, default=None, nullable=True, auto_increment=False, unique=False, prikey=False, comment=""):
 		self.name = name
 		self.data_type = data_type
+		self.default = default
 		self.nullable = nullable
 		self.auto_increment = auto_increment
+		self.unique = unique
 		self.prikey = prikey
+		self.comment = comment
 	
 	def get_construct_str(self):
 		"""
 		name data_type auto_increment pkey
-		runoob_id [NOT NULL] [INT UNSIGNED] [AUTO_INCREMENT] [PRIMARY KEY]
+		runoob_id [NOT NULL] [DEFAULT] 
+		[INT UNSIGNED] [AUTO_INCREMENT] [UNIQUE KEY] [PRIMARY KEY]
+		[COMMENT `string`]
 		"""
 		null_str = "NOT NULL" if not self.nullable else ""
+		default_str = "DEFAULT %s" % self.default if self.default else ""
+
 		auto_str = "AUTO_INCREMENT" if self.auto_increment else ""
+		unique_str = "UNIQUE KEY" if self.unique else ""
 		pkey_str = "PRIMARY KEY" if self.prikey else ""
-		cons_str = "`%s` %s %s %s %s" % (self.name, null_str, self.data_type, auto_str, pkey_str)
+
+		comm_str = "COMMENT `%s`" % self.comment if self.comment else ""
+
+		cons_str = " `%s` %s %s " % (self.name, null_str, default_str)
+		cons_str += " %s %s %s %s " % (self.data_type, auto_str, unique_str, pkey_str)
+		cons_str += " %s " % (comm_str)
 		return cons_str
 
 
@@ -33,18 +48,16 @@ class DBTable(object):
 		self.name = name
 		self.covered = covered
 		self.prikey = None
-		self.table_data = {}
-
-	def __setattr__(self, name, value):
+		self.table_data = collections.OrderedDict()
+		
+	def add_col(self, value):
 		if isinstance(value, DBColumn):
 			if value.prikey:
 				if self.prikey:
 					print("EXC 存在多个主键")
 				else:
 					self.prikey = value
-			self.table_data[name] = value
-		else:
-			object.__setattr__(self, name, value)
+			self.table_data[value.name] = value
 
 	def get_construct_str(self):
 		"""
@@ -59,10 +72,10 @@ class DBTable(object):
 		create_tail = ")ENGINE=InnoDB DEFAULT CHARSET=utf8;"
 
 		create_body = ""
-		for val in self.table_data.iteritems():
+		for val in self.table_data.values():
 			create_body += val.get_construct_str() + ',\n'
 
-		create_str = create_head + create_body[:-2] + create_tail
+		create_str = create_head + '\n' + create_body[:-2] + '\n' + create_tail
 		return create_str
 
 	def get_cursor(self):
@@ -91,5 +104,12 @@ class MySQLDB(object):
 	def get_cursor():
 		pass
 
-if __name__ == "__main__":
-	print(DataType.DBDataType.TINYINT(True))
+
+def test():
+	table = DBTable("testTable", False)
+	table.add_col(DBColumn("id", DBDataType.INT(), auto_increment=True, prikey=True))
+	table.add_col(DBColumn("name", DBDataType.VARCHAR(100)))
+	table.add_col(DBColumn("email", DBDataType.VARCHAR(32)))
+	table.add_col(DBColumn("addres", DBDataType.VARCHAR(120)))
+	print(table.get_construct_str())
+	# print(DataType.DBDataType.TINYINT(True))
---
+++ TestDB.py
@@ -0,0 +1,5 @@
+# -*- coding: UTF-8 -*-
+
+from GameDB import MySQLDB
+
+MySQLDB.test()
